674840bc4d0dbdd8600b2774637954aed528a448
Alper Turhan <alper34_44@hotmail.com>
2024-04-22 15:20:44 +0300

    Fixed build

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,15 @@
 import { defineConfig } from "vite";
+import { resolve } from 'path';
 
 export default defineConfig({
-    base: "/Create_Account_Website",
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, "index.html"),
+                profile: resolve(__dirname, "profile.html")
+            }
+        },
+        outDir: './dist'
+    },
+    base: "/Create_Account_Website"
 });
